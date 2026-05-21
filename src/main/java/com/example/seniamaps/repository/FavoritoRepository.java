598a07566466fa97c3894e91a6b47da4afa80e9f
elmotche <marponlac@gmail.com>
2026-05-21 20:21:40 +0200

    Favoritos

---
+++ src/main/java/com/example/seniamaps/repository/FavoritoRepository.java
@@ -0,0 +1,14 @@
+package com.example.seniamaps.repository;
+
+import com.example.seniamaps.entity.Favorito;
+import com.example.seniamaps.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FavoritoRepository extends JpaRepository<Favorito, Long> {
+    // Busca todos los favoritos asociados a la entidad de un usuario concreto
+    List<Favorito> findByUsuario(Usuario usuario);
+}
